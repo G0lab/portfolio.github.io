422dcf21ede7fc372d84c8905bed917b21d8ff3c
Gaurav Singh Rawat <gauravrawat061@gmail.com>
2022-03-30 15:21:46 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const home = document.getElementById("home");
+home.addEventListener("click",function()
+{ location.replace("./index.html")
+})
+const portfolio = document.getElementById("port");
+portfolio.addEventListener("click",function()
+{ location.replace("./portfolio.html")
+})
+const about = document.getElementById("about");
+about.addEventListener("click",function()
+{ location.replace("./about.html")
+})
+const contact = document.getElementById("contact");
+contact.addEventListener("click",function()
+{ location.replace("./contact.html")
+})
